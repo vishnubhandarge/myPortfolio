ca3d4417bfba8f1d9a0bfd5fb10387a02199a3e8
vishnubhandarge <vishnubhandarge@icloud.com>
2024-12-06 11:41:13 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
