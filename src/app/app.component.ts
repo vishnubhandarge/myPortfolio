bd3b4b368cfc2651563133a948e77dfea149e436
vishnubhandarge <vishnubhandarge@icloud.com>
2024-12-06 13:50:01 +0530

    Updated html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomePageComponent } from "./components/home-page/home-page.component";
 import { PortfolioComponent } from "./components/portfolio/portfolio.component";
 
 
